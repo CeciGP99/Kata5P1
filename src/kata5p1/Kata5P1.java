7fdb79f898691cb85e3d7a6eab8cdddefde0da24
User <User@DESKTOP-PUEKB8G>
2021-12-30 11:55:37 +0000

    Kata 5 parte 1 version 3

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,35 +1,16 @@
 package kata5p1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import static kata5p1.CreateTable.createNewTable;
+import java.util.List;
 
 public class Kata5P1 {
 
     public static void main(String[] args) {
-        createNewTable();
-        connect();
-        SelectApp app = new SelectApp();
-        app.selectAll();
-    }
-     
-    private static void connect(){
-        Connection conn = null;
-        try {
-            String url = "jdbc:sqlite:KATA5.db";
-            conn = DriverManager.getConnection(url);
-            System.out.println("Connexión a SQLite establecida");
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }finally{
-            try{
-                if (conn != null){
-                    conn.close();
-                }
-            }catch (SQLException ex){
-                System.out.println(ex.getMessage());
-            }
-        }
+        
+        MailListReader mlr = new MailListReader();
+        List<String> list = mlr.read("email.txt");
+        SelectApp app = new SelectApp("mail");
+        app.connect();
+        app.createNewTable();
+        app.insertData(list);       
     }
 }
--- src/kata5p1/SelectApp.java
+++ src/kata5p1/SelectApp.java
@@ -2,14 +2,25 @@ package kata5p1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SelectApp {
-    private Connection connect() {
+    
+    private final String name;
+    
+    public SelectApp(String name) {
+        this.name = name;
+    }
+    
+    public Connection connect() {
 
-        String url = "jdbc:sqlite:KATA5.db";
+        String url = "jdbc:sqlite:" + name;
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
@@ -32,6 +43,64 @@ public class SelectApp {
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        }
+    }
+    
+    public void createNewTable(){
+        Connection conn = null;
+        String sql = "CREATE TABLE IF NOT EXISTS direcc_email (\n"
+                    + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                    + " direccion text NOT NULL);";
+        conn = this.connect();
+        Statement stmt;
+        
+        try {
+            stmt = (Statement) conn.createStatement();
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void Query(){
+        Connection conn = null;
+        String sql = "SELECT * FROM PEOPLE";
+        try {
+            conn = this.connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+            System.out.println(rs.getInt("Id")+ "\t"
+                                + rs.getString("Name")+"\t"
+                                + rs.getString("Apellidos")+"\t"
+                                + rs.getString("Departamento")+"\t");
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    } 
+    
+    public void insertData (List<String> email){
+        Connection conn = null;
+        String sql = "INSERT INTO direcc_email(direccion) VALUES (?)";
+        try {
+            conn = this.connect();
+            for (String string : email) {
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                pstmt.setString(1, string);
+                pstmt.executeUpdate();
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(SelectApp.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
